5814df81bbf7d675783d56b0a91a9f30bd75d8e0
nivleM_ <46630582+nivleM-ed@users.noreply.github.com>
2019-10-05 07:32:24 +0800

    Create 20191005-0729-InitialUser.js
    not used yet

---
+++ server/seeders/20191005-0729-InitialUser.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkInsert('User', [{
+//         userame: 'admin',
+//         firstname: 'admin',
+//         lastName: 'admin',
+//         email: '',
+//         createdAt: new Date(),
+//         updatedAt: new Date()
+//       }], {});
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('User', null, {});
+//   }
+// };
